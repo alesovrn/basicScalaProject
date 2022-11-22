a147b5aae9c25ab2f228858915d46e103dc03401
Amy <ale@sovrn.com>
2022-11-22 17:20:33 +0100

    adding pom

---
+++ src/main/scala/com/sovrn/Main.scala
@@ -0,0 +1,10 @@
+package com.sovrn
+
+/**
+ * @author Amy Le
+ */
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/test/scala/MainTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FlatSpec
+
+class MainTest extends FlatSpec {
+  "The example" should "be true" in {
+    assert(1===1)
+  }
+}
